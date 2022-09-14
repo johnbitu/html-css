453a26a51ca25cb8851a34bd4f60d02e37782290
johnbitu <111550615+johnbitu@users.noreply.github.com>
2022-09-14 19:03:56 -0300

    scroll smooth

---
+++ tests/sitezin do bom/js/app.js
@@ -0,0 +1,24 @@
+const menuItens = document.querySelectorAll('.menu a[href^="#"]')
+
+menuItens.forEach(item =>{
+    item.addEventListener('click', scrollToIdOnClick)
+})
+
+function scrollToIdOnClick(event){
+    event.preventDefault();
+    const to = getScrollTopByHref(event.target) - 80;
+
+    scrollToPosition(to);
+}
+
+function scrollToPosition(to) {
+    window.scroll({
+        top: to,
+        behavior: "smooth",
+    });
+}
+
+function getScrollTopByHref(element){
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+}
